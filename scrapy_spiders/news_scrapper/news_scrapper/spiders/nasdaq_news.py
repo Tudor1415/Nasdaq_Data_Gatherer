614eb6fd90d25a859e5e774f5e529b8b3ebeec26
Tudor <tudoropran2@gmail.com>
2020-06-14 20:53:49 +0200

    debuging manager

--- scrapy_spiders/news_scrapper/news_scrapper/spiders/nasdaq_news.py
+++ scrapy_spiders/news_scrapper/news_scrapper/spiders/nasdaq_news.py
@@ -10,21 +10,27 @@ class NasdaqNewsSpider(scrapy.Spider):
     def __init__(self, symbol='', **kwargs):
         self.symbol = symbol
         self.start_urls = json.loads(requests.get(f"http://127.0.0.1:8000/info/nasdaq_historical_news_links/{symbol}").text)["Link"]
-        super().t__init__(**kwargs) 
 
     def parse(self, response):
         text = "".join(response.css("div.body:nth-child(3) > div:nth-child(2)").css("p::text").getall())
         title = response.css("h1 > span::text").get()
         published_date = response.css("time::text").get()
         return_dict = {
-            'Content': text,
-            'Title': title,
-            'Published_Date': published_date,
-            'Link': response.request.url
+            'Content': [text],
+            'Title': [title],
+            'Published_Date': [published_date],
+            'Link': [response.request.url]
         }
-        past_data = open(f"{self.symbol}.json", "w+").read()
+        try:
+            past_data = json.loads(open(f"{self.symbol}.json", "r+").read())
+        except:
+            past_data = False
         if past_data:
-            final = return_dict.update(past_data)
-            open(f"{self.symbol}.json", "w+").write(json.dumps(final))
+            past_data['Content'].append(return_dict['Content'])
+            past_data['Title'].append(return_dict['Title'])
+            past_data['Published_Date'].append(return_dict['Published_Date'])
+            past_data['Link'].append(return_dict['Link'])
+
+            open(f"{self.symbol}.json", "w+").write(json.dumps(past_data))
         else:
             open(f"{self.symbol}.json", "w+").write(json.dumps(return_dict))
--- scripts/managers/run_historical_nasdaq_scrapy.py
+++ scripts/managers/run_historical_nasdaq_scrapy.py
@@ -5,6 +5,6 @@ import json
 symbols = json.loads(requests.get("http://127.0.0.1:8000/info/nasdaq_100").text)["symbol"]
 
 for symbol in symbols:
+    open("logs.txt", "w+").write(f"Working on {symbol}")
     os.chdir("../../scrapy_spiders/news_scrapper/")
     os.system(f"scrapy crawl nasdaq_news -a symbol={symbol}")
-    open("logs.txt", "w+").write(f"Working on {symbol}")
