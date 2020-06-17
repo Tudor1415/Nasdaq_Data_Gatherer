4335d2de911e209c8778c8ec2e2cdd55250636b3
Tudor <tudoropran2@gmail.com>
2020-06-17 22:31:26 +0200

    changes

--- scripts/managers/run_google_news_scrapy.py
+++ scripts/managers/run_google_news_scrapy.py
@@ -2,14 +2,18 @@ import os
 from datetime import datetime
 
 def get_hour(now, future):
-    return (now + future) % 12
+    return (int(now) + future) % 12
 
-change_hour = 0
-open("logs.txt", "w+").write(change_hour)
+with open("logs.txt", "w+") as f:
+    f.write(datetime.now().strftime('%H'))
+os.chdir("../../scrapy_spiders/news_scrapper/")
+os.system(f"scrapy crawl google_news")
+os.system(f"scrapy crawl google_news_coverages")
 
-if datetime.now().strftime('%H') == get_hour(open("logs.txt", "w+").read(), 1):
-    change_hour = datetime.now().strftime('%H')
-    open("logs.txt", "w+").write(change_hour)
-    os.chdir("../../scrapy_spiders/news_scrapper/")
-    os.system(f"scrapy crawl google_news")
-    os.system(f"scrapy crawl google_news_coverages")
+while True:
+        if datetime.now().strftime('%H') == get_hour(open("logs.txt", "r+").read(), 1):
+                change_hour = datetime.now().strftime('%H')
+                open("logs.txt", "w+").write(str(change_hour))
+                os.chdir("../../scrapy_spiders/news_scrapper/")
+                os.system(f"scrapy crawl google_news")
+                os.system(f"scrapy crawl google_news_coverages")
