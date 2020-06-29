9b7c72df5fab4ae5a823d22eea154243490ef3b2
Tudor <tudoropran2@gmail.com>
2020-06-29 15:43:24 +0200

    added profile csv file

--- scripts/get_json_data.py
+++ scripts/get_json_data.py
@@ -19,47 +19,42 @@ CIK = df["CIK"]
 print(df.head())
 for symbol in symbols:
     try:
-        print(symbol)
-        try:
-            os.mkdir(f'../DATA/data_per_symbol/{symbol}')
-        except:
-            pass
         profile = get_company_profile(symbol)
         # dict = get_wikipedia_search_data_for_companies([profile["CompanyName"]])
-        profile.update(dict)
-        with open(f'../DATA/data_per_symbol/{symbol}/profile.json', 'w+') as f:
+        # profile.update(dict)
+        with open(f'../DATA/data_per_symbol/{symbol}_profile.json', 'w+') as f:
             f.write(json.dumps(profile))
         
         historical_data = get_historical(symbol, "2020-01-01", "2020-06-10")
-        with open(f'../DATA/data_per_symbol/{symbol}/historical_data.json', 'w+') as f:
+        with open(f'../DATA/data_per_symbol/{symbol}_historical_data.json', 'w+') as f:
             f.write(json.dumps(historical_data))
 
         summary = get_company_summary(symbol)
-        with open(f'../DATA/data_per_symbol/{symbol}/summary.json', 'w+') as f:
+        with open(f'../DATA/data_per_symbol/{symbol}_summary.json', 'w+') as f:
             f.write(json.dumps(summary))
 
         yahoo_risk = get_yahoo_risk_analysis(symbol)
-        with open(f'../DATA/data_per_symbol/{symbol}/yahoo_risk.json', 'w+') as f:
+        with open(f'../DATA/data_per_symbol/{symbol}_yahoo_risk.json', 'w+') as f:
             f.write(json.dumps(yahoo_risk))
 
         yahoo_earnings_estimate = get_yahoo_earnings_estimate(symbol)
-        with open(f'../DATA/data_per_symbol/{symbol}/yahoo_earnings_estimate.json', 'w+') as f:
+        with open(f'../DATA/data_per_symbol/{symbol}_yahoo_earnings_estimate.json', 'w+') as f:
             f.write(json.dumps(yahoo_earnings_estimate))
 
         yahoo_revenue_estimate = get_yahoo_revenue_estimate(symbol)
-        with open(f'../DATA/data_per_symbol/{symbol}/yahoo_revenue_estimate.json', 'w+') as f:
+        with open(f'../DATA/data_per_symbol/{symbol}_yahoo_revenue_estimate.json', 'w+') as f:
             f.write(json.dumps(yahoo_revenue_estimate))
 
         yahoo_growth_estimate = get_yahoo_growth_estimate(symbol)
-        with open(f'../DATA/data_per_symbol/{symbol}/yahoo_growth_estimate.json', 'w+') as f:
+        with open(f'../DATA/data_per_symbol/{symbol}_yahoo_growth_estimate.json', 'w+') as f:
             f.write(json.dumps(yahoo_growth_estimate))
 
         major_holders = get_yahoo_major_holders(symbol)
-        with open(f'../DATA/data_per_symbol/{symbol}/major_holders.json', 'w+') as f:
+        with open(f'../DATA/data_per_symbol/{symbol}_major_holders.json', 'w+') as f:
             f.write(json.dumps(major_holders))
 
         institutional_holders = get_nasdaq_institutional_holders(symbol)
-        with open(f'../DATA/data_per_symbol/{symbol}/institutional_holders.json', 'w+') as f:
+        with open(f'../DATA/data_per_symbol/{symbol}_institutional_holders.json', 'w+') as f:
             f.write(json.dumps(institutional_holders))
         # institutional_holders = json.loads(open(f'../DATA/data_per_symbol/{symbol}/institutional_holders.json', 'r+').read())
         # holders_detailed_profile = get_wikipedia_search_data_for_institution(institutional_holders[2]['OWNER_NAME'])
@@ -71,5 +66,6 @@ for symbol in symbols:
             f.write(json.dumps(edgar_8k_data))
 
         print(f"DONE {symbol}!")
-    except:
-        pass
+    except Exception as e:
+        print(e)
+
---
+++ scripts/get_profile_data.py
@@ -0,0 +1,63 @@
+import argparse
+import os
+from collections import defaultdict
+
+import pandas as pd
+
+from helpers import *
+
+
+def get_symbols(names=[], slices=""):
+    global symbols; global CIK
+    df = pd.read_csv("../DATA/cik_ticker.csv", sep="|")
+    df = df.loc[df["Exchange"] == "NASDAQ"]
+    if len(names) == 0 and len(slices) == 0:
+        symbols = df["Ticker"]
+        CIK = df["CIK"]
+    if len(names) > 0:
+        symbols = names
+        CIK = [df.loc[df["Ticker"] == i]["CIK"] for i in names]
+
+def get_profile_to_csv(file_name):
+    tmp = defaultdict(list)
+
+    for symbol in symbols:
+        try:
+            profile = get_company_profile(symbol)
+            # dict_wiki = get_wikipedia_search_data_for_companies([profile["CompanyName"]])
+            institutional_holders = get_nasdaq_institutional_holders(symbol)[2]
+            tmp["ModuleTitle"].append(profile["ModuleTitle"])
+            tmp["CompanyName"].append(profile["CompanyName"])
+            tmp["Symbol"].append(profile["Symbol"])
+            tmp["Address"].append(profile["Address"])
+            tmp["Phone"].append(profile["Phone"])
+            tmp["Industry"].append(profile["Industry"])
+            tmp["Sector"].append(profile["Sector"])
+            tmp["Region"].append(profile["Region"])
+            # tmp["Number_of_employees"].append(dict_wiki["Number_of_employees"])
+            # tmp["Subsidiaries"].append(dict_wiki["Subsidiaries"])
+            # tmp["CompanyDescription"].append(profile["CompanyDescription"])k
+            tmp["KeyExecutives"].append([(i["name"], i["title"]) for i in profile["KeyExecutives"]])
+            ih = []
+            for idx, value in enumerate(institutional_holders["OWNER_NAME"]):
+                ih.append((value, institutional_holders["MARKET_VALUE"][idx]))
+            tmp["Institutional Holders"].append(ih)
+            profile = pd.DataFrame.from_dict(tmp)
+            print(f"Done {symbol}")
+            # if file_name in os.listdir("../DATA/"):
+            #     old_df = pd.read_csv(f"../DATA/{file_name}", sep="|")
+            #     old_df = pd.concat([old_df, profile], axis=0)
+            #     old_df = old_df.Symbol.drop_duplicates()
+            #     old_df.to_csv(f"../DATA/{file_name}", sep="|", index=False)
+            # else:
+            #     profile.to_csv(f"../DATA/{file_name}", sep="|", index=False)
+        except:
+            print("ERROR")
+
+    return profile
+
+if __name__ == "__main__":
+    symbols=[]; CIK=[]
+    get_symbols()
+    profiles = get_profile_to_csv("profiles.csv")
+    profiles.to_csv(f"../DATA/profiles.csv", sep="|", index=False)
